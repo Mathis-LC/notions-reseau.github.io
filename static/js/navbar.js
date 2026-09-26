fd05e259abc83b4b4c5f529754c6af79f01b77ad
Mathis-LC <mathislc91@gmail.com>
2026-09-26 12:25:29 +0200

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   cidr.html 	modified:   classes.html 	modified:   comparaison_modele.html 	modified:   index.html 	modified:   ip_prive.html 	modified:   modele_osi.html 	modified:   modele_tcp.html 	new file:   static/js/navbar.js 	new file:   static/styles/style.css 	deleted:    styles/styles.css 	modified:   wildcard_avance.html 	modified:   wildcard_simple.html

---
+++ static/js/navbar.js
@@ -0,0 +1,18 @@
+// Ce JavaScript permet de gérer l'ouverture et la fermeture du menu de navigation sur téléphone.
+
+const burgers = document.querySelectorAll('.navbar-burger');
+
+burgers.forEach(burger => {
+    burger.addEventListener('click', () => {
+
+        const menu = document.querySelector('.navbar-menu');
+
+        burger.classList.toggle('is-active');
+        menu.classList.toggle('is-active');
+
+        burger.setAttribute(
+            'aria-expanded',
+            burger.classList.contains('is-active')
+        );
+    });
+});
